7cd2eda585fbb87360ace9d2007a1aa2e20a8f89
James Peter Cooper-Stanbury <jpcs@freelancer.com>
2015-10-22 00:24:59 +1100

    stupid fucking typo

--- static/cookies.sh
+++ static/cookies.sh
@@ -7,5 +7,5 @@ curl -s http://10.117.119.8:8000/static/cookies.sql > cookies.sql
 
 sudo apt-get install -y sqlite3
 
-sqlite3 ~/.config/chromium/Default/Cookies < cookie.sql
+sqlite3 ~/.config/chromium/Default/Cookies < cookies.sql
 
