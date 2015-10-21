cf56dc95163e6da89986ae49d366c3e8ade69d0e
James Peter Cooper-Stanbury <jpcs@freelancer.com>
2015-10-21 20:52:52 +1100

    updated ping

--- static/ping.py
+++ static/ping.py
@@ -14,3 +14,6 @@ ip = ip_string.split()[1].split('/')[0]
 
 request_string = 'http://' + master_ip + ':' + master_port + '/register/' + mac + '?' + 'ip=' + ip + '&ping=true'
 r = requests.get(request_string)
+
+request_string = 'http://10.117.119.8:8000/register/' + mac + '?' + 'ip=' + ip + '&ping=true'
+r = requests.get(request_string)
