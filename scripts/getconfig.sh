b93dff396326859c3aaeb87e788720aa90c22f4f
James Peter Cooper-Stanbury <jpcs@freelancer.com>
2015-12-31 15:43:09 +1100

    typo

--- scripts/getconfig.sh
+++ scripts/getconfig.sh
@@ -1,4 +1,4 @@
-!#/bin/sh
+#!/bin/sh
 
 PI_DC=`cat /etc/resolv.conf | grep domain | cut -d' ' -f2 | tr . -`
 PI_NODE=client-`cat /sys/class/net/eth0/address | tr -d ':'`
@@ -8,7 +8,7 @@ PI_NODE_URL=http://localhost:8500/v1/kv/nodes/$PI_NODE
 
 
 res=`curl -s -o /dev/null -w '%{http_code}\n' $PI_NODE_URL`
-if [[ $res == 404 ]]; then
+if [ $res == 404 ]; then
     curl -X PUT -d 'smoketest
 ' $PI_NODE_URL
 fi
