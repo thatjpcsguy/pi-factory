043fba206613b7aa3e5c060b52cddeb04ab32577
James Peter Cooper-Stanbury <jpcs@freelancer.com>
2015-12-31 11:22:52 +1100

    cleaned up the bootstrap scripts

--- scripts/bootstrap-client.sh
+++ scripts/bootstrap-client.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-rm -Rf /tmp/consul; mkdir -p /tmp/consul; cd /tmp/consul
+PI_DC=`cat /etc/resolv.conf | grep domain | cut -d' ' -f2 | tr . -`
+PI_NODE=client-`cat /sys/class/net/eth0/address | tr -d ':'`
 
+# Get consul binary
 if ! [ -f /usr/bin/consul ]; then
+  cd /tmp
   wget -O consul.zip https://releases.hashicorp.com/consul/0.6.0/consul_0.6.0_linux_arm.zip
   unzip consul.zip
-  mv /tmp/consul/consul /usr/bin/consul
+  mv /tmp/consul /usr/bin/consul
 fi;
 
-watch "/usr/bin/consul agent -retry-join=pimaster -data-dir /tmp/consul -dc=`cat /etc/resolv.conf | grep domain | cut -d' ' -f2 | tr . -` -node=client-`cat /sys/class/net/eth0/address | tr -d ':'`" &
+mkdir -p /tmp/consul
+watch "/usr/bin/consul agent -retry-join=pimaster -data-dir /tmp/consul -dc=$PI_DC -node=$PI_NODE" &
---
+++ scripts/bootstrap-pimaster.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+#
+# Assuming we have already cloned this repository to /var/lib/pimaster
+#
+
+
+PI_BASE=/var/lib/pimaster
+PI_DC=`cat /etc/resolv.conf | grep domain | cut -d' ' -f2 | tr . -`
+PI_NODE=pimaster-`cat /sys/class/net/eth0/address | tr -d ':'`
+
+if ! [ -d $PI_BASE ]; then
+	echo "Error: Missing install base, $PI_BASE"
+	exit 1
+fi;
+
+# Get consul binary
+if ! [ -f /usr/bin/consul ]; then
+  cd /tmp
+  wget -O consul.zip https://releases.hashicorp.com/consul/0.6.0/consul_0.6.0_linux_arm.zip
+  unzip consul.zip
+  mv /tmp/consul /usr/bin/consul
+fi;
+
+# Get the consul web UI
+if ! [ -f $PI_BASE/web/index.html ]; then
+  cd /tmp
+  wget -O web.zip https://releases.hashicorp.com/consul/0.6.0/consul_0.6.0_web_ui.zip
+  unzip web.zip
+  mkdir -p $PI_BASE/web
+  mv index.html $PI_BASE/web/
+  mv static $PI_BASE/web/
+fi;
+
+rm -f $PI_BASE/web/scripts
+ln -s $PI_BASE/scripts $PI_BASE/web/scripts # Makes scripts available via the consul web server on port 8500
+
+
+# Set up consul as a daemon in rc local
+
+sudo su
+echo 'mkdir -p /tmp/consul' > /etc/rc.local
+echo 'watch "/usr/bin/consul agent -data-dir /tmp/consul -config-dir $PI_BASE/config -dc=$PI_DC -ui-dir $PI_BASE/web -client=0.0.0.0 -bootstrap-expect 1 -node=$PI_NODE -server" &' >> /etc/rc.local
+echo 'exit 0;' >> /etc/rc.local
+
+exit
