1d4d29cf83488b079c84afa622af67788ebcf1d8
James Peter Cooper-Stanbury <jpcs@freelancer.com>
2015-12-31 11:56:12 +1100

    run as root

--- scripts/bootstrap-pimaster.sh
+++ scripts/bootstrap-pimaster.sh
@@ -9,7 +9,7 @@ PI_BASE=/var/lib/pimaster
 PI_DC=`cat /etc/resolv.conf | grep domain | cut -d' ' -f2 | tr . -`
 PI_NODE=pimaster-`cat /sys/class/net/eth0/address | tr -d ':'`
 
-sudo chown pi:pi -R $PI_BASE
+chown pi:pi -R $PI_BASE
 
 if ! [ -d $PI_BASE ]; then
 	echo "Error: Missing install base, $PI_BASE"
@@ -21,7 +21,7 @@ if ! [ -f /usr/bin/consul ]; then
   cd /tmp
   wget -O consul.zip https://releases.hashicorp.com/consul/0.6.0/consul_0.6.0_linux_arm.zip
   unzip consul.zip
-  sudo mv /tmp/consul /usr/bin/consul
+  mv /tmp/consul /usr/bin/consul
 fi;
 
 # Get the consul web UI
@@ -37,14 +37,9 @@ fi;
 rm -f $PI_BASE/web/scripts
 ln -s $PI_BASE/scripts $PI_BASE/web/scripts # Makes scripts available via the consul web server on port 8500
 
-
 # Set up consul as a daemon in rc local
 
-
-
-sudo su
 echo 'mkdir -p /tmp/consul' > /etc/rc.local
 echo "watch \"/usr/bin/consul agent -data-dir /tmp/consul -config-dir $PI_BASE/config -dc=$PI_DC -ui-dir $PI_BASE/web -client=0.0.0.0 -bootstrap-expect 1 -node=$PI_NODE -server\" &" >> /etc/rc.local
 echo 'exit 0;' >> /etc/rc.local
 
-exit
