ee1fdd073b0a254dfab6b60f1225c8d6fa0702ab
James Peter Cooper-Stanbury <jpcs@freelancer.com>
2016-01-13 20:25:44 +1100

    nuc service

---
+++ scripts/nuc-tv.sh
@@ -0,0 +1,6 @@
+if ! [ -f /etc/apt/sources.list.d/google-chrome.list ]; then  
+    wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+    sudo sh -c 'echo \"deb http://dl.google.com/linux/chrome/deb/ stable main\" >> /etc/apt/sources.list.d/google-chrome.list'
+    sudo apt-get update
+    sudo apt-get install google-chrome-stable
+fi;
