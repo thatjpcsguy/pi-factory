d3379a066388eb7e9f0541b3baa9341aa6181d8e
James Peter Cooper-Stanbury <jpcs@freelancer.com>
2016-01-13 21:24:44 +1100

    remove old configs

--- scripts/getconfig.sh
+++ scripts/getconfig.sh
@@ -19,13 +19,13 @@ PI_NODE=`hostname`
 
 PI_NODE_URL=http://localhost:8500/v1/kv/nodes/$PI_NODE
 
-
 res=`curl -s -o /dev/null -w '%{http_code}\n' $PI_NODE_URL`
 if [ "$res" == "404" ]; then
     curl -X PUT -d 'smoketest
 ' $PI_NODE_URL
 fi
 
+rm -rf $PI_BASE/config/*
 
 curl -s $PI_NODE_URL\?raw | while read line
 do
