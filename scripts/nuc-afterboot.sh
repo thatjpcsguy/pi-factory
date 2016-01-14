46cda021c6707647914649fe88487bc37b418b5d
James Peter Cooper-Stanbury <jpcs@freelancer.com>
2016-01-14 13:06:23 +1100

    added afterboot

--- scripts/nuc-afterboot.sh
+++ scripts/nuc-afterboot.sh
@@ -5,7 +5,7 @@ CHROME_URL=`curl -s http://localhost:8500/v1/kv/urls/$PI_NODE?raw`
 
 sleep 30
 
-chromium-browser --ignore-certificate-errors --disable-restore-session-state --disable-infobars --disable-session-crashed-bubble --kiosk $CHROME_URL
+chromium-browser --ignore-certificate-errors --disable-restore-session-state --disable-infobars --disable-session-crashed-bubble --kiosk $CHROME_URL &
 
 echo lol > /home/freelancer/lol.txt
 
