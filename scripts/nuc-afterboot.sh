596d6ba837f547baabf4bdf896b8bf9153dc0c40
James Peter Cooper-Stanbury <jpcs@freelancer.com>
2016-01-14 13:18:35 +1100

    added sleep to afterboot

--- scripts/nuc-afterboot.sh
+++ scripts/nuc-afterboot.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
+sleep 60
+
 PI_NODE=`hostname`
 CHROME_URL=`curl -s http://localhost:8500/v1/kv/urls/$PI_NODE?raw`
-
-sleep 30
 
 chromium-browser --ignore-certificate-errors --disable-restore-session-state --disable-infobars --disable-session-crashed-bubble --kiosk $CHROME_URL &
 
