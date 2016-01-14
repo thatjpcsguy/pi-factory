4a9510764765a4593b7a56d1ae517ef7327a537a
James Peter Cooper-Stanbury <jpcs@freelancer.com>
2016-01-14 13:11:27 +1100

    added some logging

--- scripts/nuc-afterboot.sh
+++ scripts/nuc-afterboot.sh
@@ -7,5 +7,5 @@ sleep 30
 
 chromium-browser --ignore-certificate-errors --disable-restore-session-state --disable-infobars --disable-session-crashed-bubble --kiosk $CHROME_URL &
 
-echo lol > /home/freelancer/lol.txt
+echo "Chrome Url: $CHROME_URL" >> /home/freelancer/log.txt
 
