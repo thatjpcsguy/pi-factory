ebcd45d19749f0450a826a4ca6ee9af4ddade16f
Joshua Elliott <jelliott@outlook.com>
2016-01-14 14:28:11 +1100

    --chrome-frame

--- scripts/nuc-afterboot.sh
+++ scripts/nuc-afterboot.sh
@@ -7,8 +7,8 @@ gsettings set org.gnome.desktop.session idle-delay 0
 PI_NODE=`hostname`
 CHROME_URL=`curl -s http://localhost:8500/v1/kv/urls/$PI_NODE?raw`
 
-chromium-browser --window-size=1920,1080 --chrome-frame --window-position=0,0 --ignore-certificate-errors --disable-restore-session-state --disable-infobars --disable-session-crashed-bubble --app="$CHROME_URL" &
-chromium-browser --window-size=1920,1080 --chrome-frame --window-position=0,1080 --ignore-certificate-errors --disable-restore-session-state --disable-infobars --disable-session-crashed-bubble --app="$CHROME_URL" &
+chromium-browser --window-size=640,480 --chrome-frame --window-position=0,0 --ignore-certificate-errors --disable-restore-session-state --disable-infobars --disable-session-crashed-bubble --app="$CHROME_URL" &
+chromium-browser --window-size=640,480 --chrome-frame --window-position=400,400 --ignore-certificate-errors --disable-restore-session-state --disable-infobars --disable-session-crashed-bubble --app="$CHROME_URL" &
 
 
 echo `date`" - Chrome Url: $CHROME_URL" > /home/freelancer/log.txt
