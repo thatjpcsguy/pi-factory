b909c1a2f51d68e8cc501eacc702342216cdd39c
Joshua Elliott <jelliott@outlook.com>
2016-01-14 14:45:15 +1100

    sleep between chrome

--- scripts/nuc-afterboot.sh
+++ scripts/nuc-afterboot.sh
@@ -8,6 +8,7 @@ PI_NODE=`hostname`
 CHROME_URL=`curl -s http://localhost:8500/v1/kv/urls/$PI_NODE?raw`
 
 chromium-browser --window-size=640,480 --chrome-frame --window-position=0,0 --new-window --ignore-certificate-errors --disable-restore-session-state --disable-infobars --disable-session-crashed-bubble --app="$CHROME_URL" &
+sleep 10
 chromium-browser --window-size=640,480 --chrome-frame --window-position=600,400 --new-window --ignore-certificate-errors --disable-restore-session-state --disable-infobars --disable-session-crashed-bubble --app="$CHROME_URL" &
 
 
