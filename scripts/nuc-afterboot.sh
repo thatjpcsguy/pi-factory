a27346f13e9fa31b3867499b798ef4c153b957af
Joshua Elliott <jelliott@outlook.com>
2016-01-14 14:15:12 +1100

    try chrome with an app x2

--- scripts/nuc-afterboot.sh
+++ scripts/nuc-afterboot.sh
@@ -7,7 +7,9 @@ gsettings set org.gnome.desktop.session idle-delay 0
 PI_NODE=`hostname`
 CHROME_URL=`curl -s http://localhost:8500/v1/kv/urls/$PI_NODE?raw`
 
-chromium-browser --ignore-certificate-errors --disable-restore-session-state --disable-infobars --disable-session-crashed-bubble --kiosk $CHROME_URL &
+chromium-browser --window-size=1920,1080 --window-position=0,0 --ignore-certificate-errors --disable-restore-session-state --disable-infobars --disable-session-crashed-bubble --app="$CHROME_URL" &
+chromium-browser --window-size=1920,1080 --window-position=0,1080 --ignore-certificate-errors --disable-restore-session-state --disable-infobars --disable-session-crashed-bubble --app="$CHROME_URL" &
+
 
 echo "Chrome Url: $CHROME_URL" >> /home/freelancer/log.txt
 
