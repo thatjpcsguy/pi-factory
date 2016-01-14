aeb63ee922145ce16001e8f7cc5c326905aa19b7
Joshua Elliott <jelliott@outlook.com>
2016-01-14 14:58:38 +1100

    lower kiosky

--- scripts/nuc-afterboot.sh
+++ scripts/nuc-afterboot.sh
@@ -9,9 +9,9 @@ CHROME_URL=`curl -s http://localhost:8500/v1/kv/urls/$PI_NODE?raw`
 
 mkdir -p /tmp/chrome1
 mkdir -p /tmp/chrome2
-chromium-browser --window-size=640,480 --chrome-frame --window-position=0,0 --user-data-dir=/tmp/chrome1 --ignore-certificate-errors --disable-restore-session-state --disable-infobars --disable-session-crashed-bubble --start-fullscreen --app="$CHROME_URL" &
+chromium-browser --window-size=640,480 --chrome-frame --window-position=0,0 --user-data-dir=/tmp/chrome1 --ignore-certificate-errors --disable-restore-session-state --disable-infobars --disable-session-crashed-bubble --kiosk --app="$CHROME_URL" &
 sleep 10
-chromium-browser --window-size=640,480 --chrome-frame --window-position=0,1080 --user-data-dir=/tmp/chrome2 --ignore-certificate-errors --disable-restore-session-state --disable-infobars --disable-session-crashed-bubble --start-fullscreen --app="$CHROME_URL" &
+chromium-browser --window-size=640,480 --chrome-frame --window-position=0,1200 --user-data-dir=/tmp/chrome2 --ignore-certificate-errors --disable-restore-session-state --disable-infobars --disable-session-crashed-bubble --kiosk --app="$CHROME_URL" &
 
 
 echo `date`" - Chrome Url: $CHROME_URL" > /home/freelancer/log.txt
