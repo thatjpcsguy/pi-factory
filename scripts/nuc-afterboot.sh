6ba4734195128479b78ece094a9a41f6bc1a9183
Nick Hughes <nhughes@freelancer.com>
2017-05-03 18:44:12 +1000

    Fix typo

--- scripts/nuc-afterboot.sh
+++ scripts/nuc-afterboot.sh
@@ -13,14 +13,14 @@ CHROME_URL_2=`curl -s http://localhost:8500/v1/kv/urls/$PI_NODE/2?raw`
 mkdir -p $PI_BASE/tmp/chrome1/Default/
 mkdir -p $PI_BASE/tmp/chrome2/Default/
 
-curl -s http://`dig @127.0.0.1 -p 8600 consul.service.consul +short`:8500/ui/scripts/Cookies > $PI_MASTER/tmp/chrome1/Default/Cookies
-curl -s http://`dig @127.0.0.1 -p 8600 consul.service.consul +short`:8500/ui/scripts/Cookies > $PI_MASTER/tmp/chrome2/Default/Cookies
+curl -s http://`dig @127.0.0.1 -p 8600 consul.service.consul +short`:8500/ui/scripts/Cookies > $PI_BASE/tmp/chrome1/Default/Cookies
+curl -s http://`dig @127.0.0.1 -p 8600 consul.service.consul +short`:8500/ui/scripts/Cookies > $PI_BASE/tmp/chrome2/Default/Cookies
 
-chromium-browser --window-size=640,480 --chrome-frame --window-position=0,0 --user-data-dir=$PI_MASTER/tmp/chrome1 --ignore-certificate-errors --disable-restore-session-state --disable-infobars --disable-session-crashed-bubble --kiosk --app="$CHROME_URL_1" &
+chromium-browser --window-size=640,480 --chrome-frame --window-position=0,0 --user-data-dir=$PI_BASE/tmp/chrome1 --ignore-certificate-errors --disable-restore-session-state --disable-infobars --disable-session-crashed-bubble --kiosk --app="$CHROME_URL_1" &
 sleep 1
-chromium-browser --window-size=640,480 --chrome-frame --window-position=1920,0 --user-data-dir=$PI_MASTER/tmp/chrome2 --ignore-certificate-errors --disable-restore-session-state --disable-infobars --disable-session-crashed-bubble --kiosk --app="$CHROME_URL_2" &
+chromium-browser --window-size=640,480 --chrome-frame --window-position=1920,0 --user-data-dir=$PI_BASE/tmp/chrome2 --ignore-certificate-errors --disable-restore-session-state --disable-infobars --disable-session-crashed-bubble --kiosk --app="$CHROME_URL_2" &
 
 x11vnc -auth guess -forever -loop -noxdamage -repeat -rfbport 5900 -shared -ncache 10 &
 
-echo `date`" - Chrome Url 1: $CHROME_URL_1" > $PI_MASTER/tmp/log.txt
-echo `date`" - Chrome Url 2: $CHROME_URL_2" >> $PI_MASTER/tmp/log.txt
+echo `date`" - Chrome Url 1: $CHROME_URL_1" > $PI_BASE/tmp/log.txt
+echo `date`" - Chrome Url 2: $CHROME_URL_2" >> $PI_BASE/tmp/log.txt
